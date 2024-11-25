1df24f49818e73795f0537859193230594308776
Victorcem <victorcem@hotmail.com>
2024-11-25 13:31:37 -0300

    First commit XD

---
+++ rest-with-spring-java/src/main/java/br/com/victordev/rest_with_spring_java/RestWithSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.victordev.rest_with_spring_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringJavaApplication.class, args);
+	}
+
+}
---
+++ rest-with-spring-java/src/test/java/br/com/victordev/rest_with_spring_java/RestWithSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victordev.rest_with_spring_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
